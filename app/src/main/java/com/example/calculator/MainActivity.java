fc46604ded435b6bc88373824f4b1a5e265ba719
Mishal-NAEEM <bitf17a566@pucit.edu.pk>
2021-01-18 16:43:16 +0500

    done

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView Screen;
+    private Button clear,del,add,sub,mul,div,btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,dot,Ans,power;
+    private String input="",Answer;
+    private boolean clearResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Screen=findViewById(R.id.screen);
+
+        clear=findViewById(R.id.clear);
+        del=findViewById(R.id.del);
+        add=findViewById(R.id.add);
+        sub=findViewById(R.id.sub);
+        div=findViewById(R.id.div);
+        mul=findViewById(R.id.mul);
+        dot=findViewById(R.id.dot);
+        Ans=findViewById(R.id.Ans);
+        power=findViewById(R.id.power);
+
+        btn0=findViewById(R.id.btn0);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+        btn4=findViewById(R.id.btn4);
+        btn5=findViewById(R.id.btn5);
+        btn6=findViewById(R.id.btn6);
+        btn7=findViewById(R.id.btn7);
+        btn8=findViewById(R.id.btn8);
+        btn9=findViewById(R.id.btn9);
+
+    }
+    public void ButtonClick(View view){
+        Button button= (Button) view;
+        String data=button.getText().toString();
+        switch (data){
+            case "AC":
+                input="";
+                break;
+            case "Ans":
+                clearResult=false;
+                input+=Answer;
+                break;
+            case "x":
+                clearResult=false;
+                Solve();
+                input+="*";
+                break;
+            case "^":
+                clearResult=false;
+                Solve();
+                input+="^";
+                break;
+            case "=":
+                clearResult=true;
+                Solve();
+                Answer=input;
+                break;
+            case "del":
+                if(input.length()>0){
+                    clearResult=false;
+                    String newText=input.substring(0,input.length()-1);
+                    input=newText;
+                }
+                break;
+            default:
+                if(input==null){
+                    input="";
+                }
+                if(data.equals("+") || data.equals("-") || data.equals("/")){
+                    clearResult=false;
+                    Solve();
+                }
+                else if(clearResult==true){
+                    input="";
+                    clearResult=false;
+                }
+                input+=data;
+        }
+        Screen.setText(input);
+    }
+    public void Solve(){
+        if(input.split("\\*").length==2){
+            String numbers[]=input.split("\\*");
+            try{
+                double mul=Double.parseDouble(numbers[0])*Double.parseDouble(numbers[1]);
+                input=mul+"";
+            }
+            catch (Exception e){
+                //Display error
+            }
+        }
+        else if(input.split("/").length==2){
+            String numbers[]=input.split("/");
+            try{
+                double div=Double.parseDouble(numbers[0])/Double.parseDouble(numbers[1]);
+                input=div+"";
+            }
+            catch (Exception e){
+                //Display error
+            }
+        }
+        else if(input.split("\\^").length==2){
+            String numbers[]=input.split("\\^");
+            try{
+                double pow=Math.pow(Double.parseDouble(numbers[0]),Double.parseDouble(numbers[1]));
+                input=pow+"";
+            }
+            catch (Exception e){
+                //Display error
+            }
+        }
+        else if(input.split("\\+").length==2){
+            String numbers[]=input.split("\\+");
+            try{
+                double sum=Double.parseDouble(numbers[0])+Double.parseDouble(numbers[1]);
+                input=sum+"";
+            }
+            catch (Exception e){
+                //Display error
+            }
+        }
+        else if(input.split("\\-").length>1){
+            String numbers[]=input.split("\\-");
+            if(numbers[0]=="" && numbers.length==2){
+                numbers[0]=0+"";
+            }
+            try{
+                double sub=0;
+                if(numbers.length==2) {
+                    sub = Double.parseDouble(numbers[0]) - Double.parseDouble(numbers[1]);
+                }
+                else if(numbers.length==3){
+                    sub = -Double.parseDouble(numbers[1]) - Double.parseDouble(numbers[2]);
+                }
+                input=sub+"";
+            }
+            catch (Exception e){
+                //Display error
+            }
+        }
+        String n[]=input.split("\\.");
+        if(n.length>1){
+            if(n[1].equals("0")){
+                input=n[0];
+            }
+        }
+        Screen.setText(input);
+    }
+}
